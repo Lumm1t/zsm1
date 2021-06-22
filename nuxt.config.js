a681a1571fa6c5f6a25bc6b19230252a151fadf3
Lumm1t <aleksanderszolowicz@gmail.com>
2021-06-22 21:18:10 +0200

    chore(nuxt): add adm folders as components

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,11 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    components: [
+    '~/components/atoms',
+    '~/components/molecules',
+    '~/components/organisms',
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [

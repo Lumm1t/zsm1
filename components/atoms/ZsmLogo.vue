1ee91644ed95d4a1a7fa3a74a073b5b93a943b38
Lumm1t <aleksanderszolowicz@gmail.com>
2021-07-14 03:43:07 +0200

    feat(zsm logo): fix animation

--- components/atoms/ZsmLogo.vue
+++ components/atoms/ZsmLogo.vue
@@ -1,15 +1,17 @@
 <template>
-  <nuxt-link :to="'/'" tabindex="0">
-    <div
-      class="
-        font-bold
-        transform
-        hover:skew-y-6
-        focus:skew-y-6
-        transition-transform
-        duration-200
-      "
-    >
+  <nuxt-link
+    :to="'/'"
+    tabindex="0"
+    class="
+      transform
+      hover:skew-y-6
+      focus:skew-y-6
+      transition-transform
+      duration-200
+      focus:outline-none
+    "
+  >
+    <div class="font-bold">
       <div class="text-2xl leading-4 text-light-text dark:text-dark-text">
         ZSM1
       </div>

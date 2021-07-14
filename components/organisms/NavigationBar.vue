ebb94398a6e1e5cff6e3c87a0f4605706491346b
Lumm1t <aleksanderszolowicz@gmail.com>
2021-07-14 19:39:38 +0200

    feat(navbar): change padding

--- components/organisms/NavigationBar.vue
+++ components/organisms/NavigationBar.vue
@@ -1,12 +1,16 @@
 <template>
   <nav
     class="
+      fixed
+      px-6
+      xl:px-12
       navbar
       translate-y-0
-      w-full
-      mx-auto
-      fixed
-      px-10
+      container
+      flex
+      items-center
+      justify-between
+      min-w-full
       z-50
       transition-all
       duration-500
@@ -14,171 +18,81 @@
     "
     :class="{
       'transform -translate-y-full': !showNavbar,
-      'bg-light-layer dark:bg-dark-layer shadow-md xl:pt-2': showBackground,
-      'xl:pt-8': !showBackground,
+      'bg-light-layer dark:bg-dark-layer shadow-md py-6': showBackground,
+      'py-6 xl:py-16': !showBackground,
     }"
   >
-    <div class="container h-20 flex items-center justify-between min-w-full">
-      <!-- Drawer -->
-      <div class="xl:hidden">
-        <div class="flex items-center">
-          <button
-            class="text-light-text dark:text-dark-text"
-            aria-label="Open Menu"
-            @click="drawer"
-            @keyup.enter="drawer"
+    <!-- Drawer -->
+    <div class="xl:hidden">
+      <div class="flex items-center">
+        <button
+          class="text-light-text dark:text-dark-text"
+          aria-label="Open Menu"
+          @click="drawer"
+          @keyup.enter="drawer"
+        >
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            class="w-8 h-8"
           >
-            <svg
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              viewBox="0 0 24 24"
-              class="w-8 h-8"
-            >
-              <path d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-          </button>
-        </div>
+            <path d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+      </div>
 
-        <transition
-          enter-class="opacity-0"
-          enter-active-class="ease-out transition-medium"
-          enter-to-class="opacity-100"
-          leave-class="opacity-100"
-          leave-active-class="ease-out transition-medium"
-          leave-to-class="opacity-0"
+      <transition
+        enter-class="opacity-0"
+        enter-active-class="ease-out transition-medium"
+        enter-to-class="opacity-100"
+        leave-class="opacity-100"
+        leave-active-class="ease-out transition-medium"
+        leave-to-class="opacity-0"
+      >
+        <div
+          v-show="isOpen"
+          class="z-10 fixed inset-0 transition-opacity"
+          @keydown.esc="isOpen = false"
         >
           <div
-            v-show="isOpen"
-            class="z-10 fixed inset-0 transition-opacity"
-            @keydown.esc="isOpen = false"
-          >
-            <div
-              class="absolute inset-0 bg-black opacity-50"
-              tabindex="0"
-              @click="isOpen = false"
-              @keyup.enter="isOpen = false"
-            ></div>
-          </div>
-        </transition>
+            class="absolute inset-0 bg-black opacity-50"
+            tabindex="0"
+            @click="isOpen = false"
+            @keyup.enter="isOpen = false"
+          ></div>
+        </div>
+      </transition>
 
-        <aside
-          class="
-            transform
-            top-0
-            left-0
-            w-64
-            text-light-text
-            dark:text-dark-text
-            bg-light-layer
-            dark:bg-dark-layer
-            fixed
-            h-full
-            overflow-auto
-            ease-in-out
-            transition-all
-            duration-300
-            z-30
-          "
-          :class="
-            isOpen ? 'translate-x-0 visible' : '-translate-x-full invisible'
-          "
-        >
-          <div v-for="(item, index) in menuItems" :key="index">
-            <a
-              v-if="item.link"
-              class="inline-block px-6 py-3 hover:text-primary"
-              :href="item.link"
-              tabindex="0"
-            >
-              {{ item.title }}
-            </a>
-
-            <div
-              v-else
-              class="px-6 py-3 hover:text-primary"
-              tabindex="0"
-              @click="dropdownHandler($event)"
-              @keyup.enter="dropdownHandler($event)"
-            >
-              <span>{{ item.title }}</span>
-              <span class="absolute ml-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z"></path>
-                  <polyline points="6 9 12 15 18 9"></polyline>
-                </svg>
-              </span>
-
-              <ul class="hidden top-0">
-                <li
-                  v-for="(element, i) in item.links"
-                  :key="i"
-                  tabindex="0"
-                  class="
-                    cursor-pointer
-                    text-light-text
-                    dark:text-dark-text
-                    tracking-normal
-                    py-3
-                    hover:bg-primary hover:text-white
-                    px-3
-                    font-normal
-                  "
-                >
-                  <a :href="element.link">
-                    {{ element.title }}
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </aside>
-      </div>
-
-      <!-- Logo -->
-      <div class="flex items-center">
-        <ZsmLogo />
-      </div>
-
-      <!-- Menu -->
-      <div
+      <aside
         class="
-          hidden
-          xl:flex
-          items-center
-          justify-center
-          sm:items-stretch sm:justify-start
-          font-semibold
+          transform
+          top-0
+          left-0
+          w-64
+          text-light-text
+          dark:text-dark-text
+          bg-light-layer
+          dark:bg-dark-layer
+          fixed
+          h-full
+          overflow-auto
+          ease-in-out
+          transition-all
+          duration-300
+          z-30
+        "
+        :class="
+          isOpen ? 'translate-x-0 visible' : '-translate-x-full invisible'
         "
       >
         <div v-for="(item, index) in menuItems" :key="index">
           <a
             v-if="item.link"
-            class="
-              px-7
-              2xl:px-12
-              block
-              leading-5
-              text-light-text
-              dark:text-dark-text
-              hover:text-primary
-              focus:text-primary focus:outline-none
-              transition-colors
-              duration-200
-              ease-in-out
-            "
+            class="inline-block px-6 py-3 hover:text-primary"
             :href="item.link"
             tabindex="0"
           >
@@ -187,69 +101,13 @@
 
           <div
             v-else
-            class="
-              px-7
-              2xl:px-12
-              items-center
-              flex
-              leading-5
-              text-light-text
-              dark:text-dark-text
-              hover:text-primary
-              focus:text-primary focus:outline-none
-              transition-colors
-              duration-200
-              ease-in-out
-              relative
-              cursor-pointer
-            "
+            class="px-6 py-3 hover:text-primary"
             tabindex="0"
             @click="dropdownHandler($event)"
             @keyup.enter="dropdownHandler($event)"
           >
-            <ul
-              class="
-                bg-light-layer
-                dark:bg-dark-layer
-                shadow
-                rounded
-                py-1
-                w-56
-                left-0
-                mt-8
-                -ml-4
-                absolute
-                hidden
-                top-0
-              "
-            >
-              <li
-                v-for="(element, i) in item.links"
-                :key="i"
-                class="
-                  cursor-pointer
-                  text-light-text
-                  dark:text-dark-text
-                  leading-3
-                  tracking-normal
-                  py-3
-                  hover:bg-primary hover:text-white
-                  focus:bg-primary focus:text-white
-                  px-3
-                  font-normal
-                  focus:outline-none
-                "
-                tabindex="0"
-              >
-                <a :href="element.link" class="focus:outline-none">
-                  {{ element.title }}
-                </a>
-              </li>
-            </ul>
-
-            {{ item.title }}
-
-            <span class="ml-2">
+            <span>{{ item.title }}</span>
+            <span class="absolute ml-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
@@ -265,14 +123,158 @@
                 <polyline points="6 9 12 15 18 9"></polyline>
               </svg>
             </span>
+
+            <ul class="hidden top-0">
+              <li
+                v-for="(element, i) in item.links"
+                :key="i"
+                tabindex="0"
+                class="
+                  cursor-pointer
+                  text-light-text
+                  dark:text-dark-text
+                  tracking-normal
+                  py-3
+                  hover:bg-primary hover:text-white
+                  px-3
+                  font-normal
+                "
+              >
+                <a :href="element.link">
+                  {{ element.title }}
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
+      </aside>
+    </div>
 
-      <!-- Dark mode button -->
-      <div class="flex items-center">
-        <DarkMode />
+    <!-- Logo -->
+    <div class="flex items-center">
+      <ZsmLogo />
+    </div>
+
+    <!-- Menu -->
+    <div
+      class="
+        hidden
+        xl:flex
+        items-center
+        justify-center
+        sm:items-stretch sm:justify-start
+        font-semibold
+      "
+    >
+      <div v-for="(item, index) in menuItems" :key="index">
+        <a
+          v-if="item.link"
+          class="
+            px-7
+            2xl:px-12
+            block
+            leading-5
+            text-light-text
+            dark:text-dark-text
+            hover:text-primary
+            focus:text-primary focus:outline-none
+            transition-colors
+            duration-200
+            ease-in-out
+          "
+          :href="item.link"
+          tabindex="0"
+        >
+          {{ item.title }}
+        </a>
+
+        <div
+          v-else
+          class="
+            px-7
+            2xl:px-12
+            items-center
+            flex
+            leading-5
+            text-light-text
+            dark:text-dark-text
+            hover:text-primary
+            focus:text-primary focus:outline-none
+            transition-colors
+            duration-200
+            ease-in-out
+            relative
+            cursor-pointer
+          "
+          tabindex="0"
+          @click="dropdownHandler($event)"
+          @keyup.enter="dropdownHandler($event)"
+        >
+          <ul
+            class="
+              bg-light-layer
+              dark:bg-dark-layer
+              shadow
+              rounded
+              py-1
+              w-56
+              left-0
+              mt-8
+              -ml-4
+              absolute
+              hidden
+              top-0
+            "
+          >
+            <li
+              v-for="(element, i) in item.links"
+              :key="i"
+              class="
+                cursor-pointer
+                text-light-text
+                dark:text-dark-text
+                leading-3
+                tracking-normal
+                py-3
+                hover:bg-primary hover:text-white
+                focus:bg-primary focus:text-white
+                px-3
+                font-normal
+                focus:outline-none
+              "
+              tabindex="0"
+            >
+              <a :href="element.link" class="focus:outline-none">
+                {{ element.title }}
+              </a>
+            </li>
+          </ul>
+
+          {{ item.title }}
+
+          <span class="ml-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z"></path>
+              <polyline points="6 9 12 15 18 9"></polyline>
+            </svg>
+          </span>
+        </div>
       </div>
+    </div>
+
+    <!-- Dark mode button -->
+    <div class="flex items-center">
+      <DarkMode />
     </div>
   </nav>
 </template>

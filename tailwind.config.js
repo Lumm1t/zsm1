0229e47d868253612a83673fa76352e7960cba61
Lumm1t <aleksanderszolowicz@gmail.com>
2021-08-23 03:10:36 +0200

    chore(tailwind): use jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: 'jit',
   darkMode: 'class',
   theme: {
     container: {
@@ -47,11 +48,6 @@ module.exports = {
       gridTemplateRows: {
         heroinfo: '1fr 2fr 1fr',
       },
-    },
-  },
-  variants: {
-    extend: {
-      opacity: ['dark'],
     },
   },
 }
